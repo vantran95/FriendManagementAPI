e9d486249383c1eaf72bd91b03e6b49f26de4398
Van Tran <van.tran@s3corp.com>
2021-05-17 09:17:29 +0700

    Add unit test for user repository

--- api/internal/repository/users/read_test.go
+++ api/internal/repository/users/read_test.go
@@ -57,3 +57,68 @@ func TestRepositoryImpl_GetUser(t *testing.T) {
 		})
 	}
 }
+
+func TestRepositoryImpl_GetAllUsers(t *testing.T) {
+	tcs := []struct {
+		scenario             string
+		mockGetAllUserOutput *[]models.User
+		mockErr              error
+		expResult            interface{}
+		expErr               error
+	}{
+		{
+			scenario: "success",
+			mockGetAllUserOutput: &[]models.User{
+				{
+					ID:    1,
+					Email: "a@gmail.com",
+				},
+				{
+					ID:    2,
+					Email: "b@gmail.com",
+				},
+			},
+			expResult: &[]models.User{
+				{
+					ID:    1,
+					Email: "a@gmail.com",
+				},
+				{
+					ID:    2,
+					Email: "b@gmail.com",
+				},
+			},
+		},
+		{
+			scenario: "do not have users",
+
+			mockErr: errors.New("do not have users"),
+			expErr:  errors.New("do not have users"),
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.scenario, func(t *testing.T) {
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			rows := sqlmock.NewRows([]string{"id", "email"})
+			if tc.mockGetAllUserOutput != nil {
+				for _, v := range *tc.mockGetAllUserOutput {
+					rows.AddRow(v.ID, v.Email)
+				}
+			}
+			query := regexp.QuoteMeta(`select id, email from users`)
+			mock.ExpectQuery(query).WillReturnRows(rows)
+
+			myDB := &RepositoryImpl{db}
+			result, _ := myDB.GetAllUsers()
+			assert.Equal(t, tc.expErr, tc.mockErr)
+			if tc.expErr == nil {
+				assert.Equal(t, tc.expResult, result)
+			}
+		})
+	}
+}
